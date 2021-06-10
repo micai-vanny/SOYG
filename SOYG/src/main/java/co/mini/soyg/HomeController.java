a73dc9948a3a5e7417bb044bb76c7081512a64e9
admin <admin@YD02-15>
2021-06-10 12:41:32 +0900

    예담 미니 프로젝트 1조 시작!

---
+++ SOYG/src/main/java/co/mini/soyg/HomeController.java
@@ -0,0 +1,15 @@
+package co.mini.soyg;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/home.do")
+	public String home(Model model) {
+		return "home";
+	}
+	
+}
