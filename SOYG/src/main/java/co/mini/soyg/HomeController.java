11b09c93bcf6da12877da1d85047a8e84be62658
admin <admin@YD02-15>
2021-06-14 17:48:50 +0900

    지역별/분야별 뷰 계속 수정 중, 메인 이미지 디렉토리에 저장.

--- SOYG/src/main/java/co/mini/soyg/HomeController.java
+++ SOYG/src/main/java/co/mini/soyg/HomeController.java
@@ -1,14 +1,23 @@
 package co.mini.soyg;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import co.mini.soyg.main.service.MainService;
+
 @Controller
 public class HomeController {
+	@Autowired
+	private MainService dao;
 	
 	@RequestMapping("/home.do")
 	public String home(Model model) {
+		// 지역별 카테고리
+		model.addAttribute("mainRegionList", dao.mainRegionList());
+		// 분야별 카테고리
+		model.addAttribute("mainFieldsList", dao.mainFieldsList());
 		return "tiles/main";
 	}
 	
--- SOYG/src/main/java/co/mini/soyg/main/service/MainService.java
+++ SOYG/src/main/java/co/mini/soyg/main/service/MainService.java
@@ -10,9 +10,21 @@ public interface MainService {
 	//비로그인 시 최근 등록 스터디 그룹 조회
 	List<MainVO> recentlyRegGroupList();
 	
+	// 메인 지역별 카테고리 리스트
+	List<MainVO> mainRegionList();
+	
+	// 메인 분야별 카테고리 리스트
+	List<MainVO> mainFieldsList();
+	
 	//지역별 스터디 아이콘 누를 시 처리
 	List<MainVO> regionList(MainVO vo);
 	
 	//분야별 스터디 아이콘 누를 시 처리
 	List<MainVO> fieldsList(MainVO vo);
+	
+	//지역별 카테고리 리스트
+	List<MainVO> regselect();
+	
+	//분야별 카테고리 리스트
+	List<MainVO> fieldsSelect();
 }
